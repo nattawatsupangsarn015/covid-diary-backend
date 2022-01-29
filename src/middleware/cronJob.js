5ef8363763ea1eb7c3779aef110720755f14ab5e
nattawatsupangsarn015 <superpower-max@hotmail.com>
2022-01-29 17:51:37 +0700

    [UPDATE] update time for cron job

--- src/middleware/cronJob.js
+++ src/middleware/cronJob.js
@@ -2,7 +2,7 @@ const scheduler = require("node-schedule");
 const covidFunc = require("../functions/covidDiary");
 const database = require("../utils/mongo");
 
-scheduler.scheduleJob("15 * * * * *", async () => {
+scheduler.scheduleJob("* * 3 * * *", async () => {
   console.time("using time: ");
   console.log("waiting for update .. ");
   await database();
